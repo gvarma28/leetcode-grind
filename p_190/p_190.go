6a2b008f801c02451b0c8e16753fa4124582cc8e
gvarma28 <gouravswork@gmail.com>
2024-10-07 23:40:20 +0530

    feat: p_190

---
+++ p_190/p_190.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// https://leetcode.com/problems/reverse-bits/description/
+func main() {
+	res := reverseBits(43261596)
+	fmt.Println(res)
+}
+
+func reverseBits(num uint32) uint32 {
+	res := uint32(0)
+	for i := 0; i < 32; i++ {
+		cur := num%2 == 0
+        res = res << 1
+		if !cur {
+			res++
+		}
+		num = num >> 1
+	}
+	return res
+}
