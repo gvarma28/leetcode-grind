c6054c57befbdd9fef2fce7ddb40b4a688cafb14
gvarma28 <gouravswork@gmail.com>
2024-10-29 22:32:45 +0530

    feat: p_228

--- p_228/p_228.go
+++ p_228/p_228.go
@@ -8,7 +8,7 @@ import (
 
 // https://leetcode.com/problems/summary-ranges/description/
 func main() {
-	res := summaryRanges([]int{0, 1, 2, 4, 5, 7})
+	res := summaryRanges([]int{0, 2, 3, 4, 6, 8, 9})
 	fmt.Println(res)
 }
 
@@ -16,32 +16,25 @@ func summaryRanges(nums []int) []string {
 	res := []string{}
 	n := len(nums)
 
+	if n == 0 {
+		return res
+	}
+
 	var str strings.Builder
-	cur := -1
-	for i := 0; i < n-1; i++ {
-		fmt.Println(nums[i], cur)
-		if nums[i+1]-nums[i] == 1 {
-			if cur == -1 {
+	firstElem := true 
+	for i := 0; i < n; i++ {
+		if i < n-1 && nums[i+1]-nums[i] == 1 {
+			if firstElem {
 				str.WriteString(strconv.Itoa(nums[i]) + "->")
-				cur = i;
+				firstElem = false
 			}
 			continue
 		}
-		if cur != -1 {
-			str.WriteString(strconv.Itoa(nums[i]))
-			res = append(res, str.String())
-			str.Reset()
-		} else {
-			temp := strconv.Itoa(nums[i])
-			str.WriteString(temp + "->" + temp)
-			res = append(res, str.String())
-			str.Reset()
-		}
-		cur = -1
+		str.WriteString(strconv.Itoa(nums[i]))
+		res = append(res, str.String())
+		str.Reset()
+		firstElem = true
 	}
-
-	fmt.Println(cur)
-
 
 	return res
 }
