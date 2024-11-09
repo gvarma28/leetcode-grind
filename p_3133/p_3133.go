ff35fe11cf3476874f55b9d8100e571dcbd21f63
gvarma28 <gouravswork@gmail.com>
2024-11-09 23:12:38 +0530

    feat: p_3133/wip

---
+++ p_3133/p_3133.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+// problem_url
+func main() {
+	res := minEnd(2, 7)
+	fmt.Println(res)
+}
+
+func minEnd(n int, x int) int64 {
+	var res int64 = 0
+
+	xbits := []int{}
+
+	for i := 0; i <= 64; i++ {
+		if (x>>i)&1 == 1 {
+			xbits = append(xbits, 1)
+		} else {
+			xbits = append(xbits, 0)
+		}
+	}
+
+	fmt.Println(xbits)
+	idx := 0
+	for i := 0; i <= 64; i++ {
+		if xbits[i] == 1 {
+			res += (1 << i)
+			continue
+		}
+		if ((n-1)>>idx)&1 == 1 {
+			res += (1 << i)
+			idx++
+		}
+	}
+
+	// for i := 0; i < n; i++ {
+	// 	for j := 0; i <= 64; j++ {
+	// 		if xbits[i] == 1 {
+	// 			continue
+	// 		}
+	// 	}
+	// }
+
+
+	return res
+
+}
