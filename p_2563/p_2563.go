43ecd05378ddaf48ef0a12b0ac74da7340355f79
gvarma28 <gouravswork@gmail.com>
2024-11-14 22:07:04 +0530

    feat: p_2563/review/binary-search

--- p_2563/p_2563.go
+++ p_2563/p_2563.go
@@ -7,45 +7,44 @@ import (
 
 // https://leetcode.com/problems/count-the-number-of-fair-pairs/description/
 func main() {
-	res := countFairPairs([]int{1,7,9,2,5}, 11, 11)
+	res := countFairPairs([]int{0, 1, 7, 4, 4, 5}, 3, 6)
 	fmt.Println(res)
 }
 
-// TLE - add binary search
 func countFairPairs(nums []int, lower int, upper int) int64 {
 	sort.Ints(nums)
 	n := len(nums)
 
-	p, q := 0, n-1
-
 	var res int64 = 0
-	for {
-		if p >= q {
-			break
-		}
+	for i := 0; i < n; i++ {
+		cur := nums[i]
 
-		var sum int64 = int64(nums[p]) + int64(nums[q])
-		if sum >= int64(lower) && sum <= int64(upper) {
-			temp := q
-			for {
-				if p >= temp {
-					break
-				}
-				var sum int64 = int64(nums[p]) + int64(nums[temp])
-				if sum >= int64(lower) && sum <= int64(upper) {
-					res++
-					temp--
-				} else {
-					break
-				}
-			}
-			p++
-		} else if sum < int64(lower) {
-			p++
-		} else {
-			q--
-		}
+		max := upper - cur
+		min := lower - cur
+
+		upperIdx := bs(nums, i+1, n-1, max+1)
+		lowerIdx := bs(nums, i+1, n-1, min)
+
+		res += int64(upperIdx) - int64(lowerIdx)
 	}
 
 	return res
+}
+
+func bs(nums []int, l int, r int, target int) int {
+	for {
+		if l > r {
+			break
+		}
+
+		var mid int = (l + r) / 2
+		if nums[mid] >= target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+
+	}
+
+	return r
 }
